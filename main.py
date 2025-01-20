691edc7d4224f87b59cc09f9e7a2f1cbf25559fd
dxv3 <davesangha9@gmail.com>
2025-01-20 03:58:41 +0000

    Blacklist & Status

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ with open("config.json", "r") as config_file:
 
 BOT_TOKEN = config["BOT_TOKEN"]
 WHITELISTED_IDS = config["WHITELISTED_IDS"]
+BLACKLISTED_IDS = config["BLACKLISTED_IDS"]
 
 intents = discord.Intents.all()
 intents.members = True
@@ -16,17 +17,22 @@ bot = commands.Bot(command_prefix='+', intents=intents)
 @bot.event
 async def on_ready():
     print(f"Bot connected as {bot.user}")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="dxv3"))
 
 @bot.command()
 async def dmall(ctx, *, message):
     if ctx.author.id in WHITELISTED_IDS:
-        members_to_message = [member for member in ctx.guild.members if not member.bot]
+        # exclude bots and blacklisted ids
+        members_to_message = [
+            member for member in ctx.guild.members 
+            if not member.bot and member.id not in BLACKLISTED_IDS
+        ]
         total_members = len(members_to_message)
         batch_size = 10
         delay_between_batches = 10
-        delay_between_messages = 1
+        delay_between_messages = 1 # make higher if shit but 1 works for servers under 200 members
 
-        status_message = await ctx.send(f"Starting DM process... 0/{total_members} messages sent")
+        status_message = await ctx.send(f"Started DMing!!! 0/{total_members} messages sent")
         sent_count = 0
 
         for i in range(0, total_members, batch_size):
@@ -37,18 +43,20 @@ async def dmall(ctx, *, message):
                     await member.send(message)
                     sent_count += 1
                     await status_message.edit(content=f"DMing in progress... {sent_count}/{total_members} messages sent")
+                    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="DM {sent_count}/{total_members}"))
+
                     await asyncio.sleep(delay_between_messages)
                 except discord.errors.Forbidden:
-                    await ctx.send(f"Unable to send a message to {member.name} (DMs disabled or blocked)!")
+                    print(f"Unable to send a message to {member.name} (DMs disabled or blocked).")
                 except discord.errors.HTTPException as e:
                     if e.status == 429:
                         print("Rate limit reached, waiting... (Error 429)")
                         retry_after = e.response.get("Retry-After", 5)
                         await asyncio.sleep(retry_after)
                     else:
-                        await ctx.send(f"HTTP error while sending a message to {member.name}: {e}")
+                        print(f"HTTP error while sending a message to {member.name}: {e}")
                 except Exception as e:
-                    await ctx.send(f"Error while sending a message to {member.name}: {e}")
+                    print(f"Error while sending a message to {member.name}: {e}")
 
             if i + batch_size < total_members:
                 print(f"Waiting {delay_between_batches} seconds before sending the next batch...")
@@ -56,12 +64,12 @@ async def dmall(ctx, *, message):
 
         await status_message.edit(content=f"DM process completed! {sent_count}/{total_members} messages sent")
     else:
-        await ctx.send("You are not authorised to use this command!")
+        await ctx.send(r"you can't use this cmd! :(")
 
 @bot.command(name="customhelp")
 async def help_command(ctx):
     help_message = (
-        "+dmall [message]: Sends a private message to all members of the server **(Whitelisted users only)**\n"
+        "+dmall [message]: Sends a DM to all members of the server **(@dxv3 only >_<)**\n"
         "+customhelp: Displays the list of available commands"
     )
     await ctx.send(help_message)
